d3830c570db7700410a07612a2b0d586f1bb343e
AlbekAnapin <77009508+AlbekAnapin@users.noreply.github.com>
2021-11-23 13:42:30 +0600

    добавлен доп проект для использования памяти

--- MyStore/IBookRepository.cs
+++ MyStore/IBookRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyStore
 {
-    interface IBookRepository
+   public interface IBookRepository
     {
         Book[] GetAllByTitle(string titlePart);
     }
